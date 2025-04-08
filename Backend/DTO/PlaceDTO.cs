21888ba0f76fb0b68f9a3bfd1868dee7ff4374bc
ThorANilsson <thor.a.nilsson@gmail.com>
2025-04-08 18:28:21 +0200

    Lade till två DTOs. Se beskrivning för förklaring
    What is a DTO?
    
    In short, a simple class that's used to pass data between layers. Maybe your domain models have a large set of properties, but you only need to display five of them to the end user. Then you map this domain object to a Dto and return that to the UI.
    
    You also never want to expose domain models directly to the end user. Mainly because changing the domain would then go through all the way to the end user directly, which could be really bad.
    
    https://www.reddit.com/r/dotnet/comments/nfk3ry/what_is_actually_a_dto_and_when_should_i_use_it/

---
+++ Backend/DTO/PlaceDTO.cs
@@ -0,0 +1,10 @@
+public class PlaceDTO
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public List<PlaceUtilityDTO>? PlaceUtilities { get; set; } // = new(); om det ska vara en tom array istället för null
+}
---
+++ Backend/DTO/PlaceUtilityDTO.cs
@@ -0,0 +1,5 @@
+public class PlaceUtilityDTO
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+}
