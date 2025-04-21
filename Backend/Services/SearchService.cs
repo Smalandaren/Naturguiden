8e6f5a18c3f613aecaede956332bc8d464b3f000
Smalandaren <isakthysell05@gmail.com>
2025-04-21 14:07:31 +0200

    Skapade första version av sök i backend
    Söker i titel (Ska söka på mer)
    Case sensitive (ska fixas)

---
+++ Backend/Services/SearchService.cs
@@ -0,0 +1,33 @@
+﻿using Backend.Data;
+using System.Collections;
+using Backend.Models;
+
+public class SearchService
+{
+    private readonly ApplicationDbContext _context;
+    private readonly PlacesService _placesService;
+
+    public SearchService(ApplicationDbContext context)
+    {
+        _context = context;
+        _placesService = new PlacesService(context);
+    }
+
+    public async Task<List<PlaceDTO>> Search(String searchTerm)
+    {
+        List<Place> matches = new List<Place>();
+        List<PlaceDTO> matchesDTO = new List<PlaceDTO>();
+
+        matches = _context.Places.Where(p => p.Name.Contains(searchTerm)).ToList();
+
+        if (matches.Count > 0)
+        {
+            for (int i = 0; i < matches.Count; i++)
+            {
+                matchesDTO.Add(await _placesService.GetAsync(matches[i].Id));
+            }
+        }
+        return matchesDTO;
+    }
+}
+
