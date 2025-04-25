a054739f6219cac3c06f24fb252e4741d7c3f13a
ThorANilsson <thor.a.nilsson@gmail.com>
2025-04-25 15:12:01 +0200

    Lade till funktionalitet på backend för att hämta en användares besökta platser

--- Backend/Controllers/ProfileController.cs
+++ Backend/Controllers/ProfileController.cs
@@ -4,16 +4,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
 {
-    [Authorize]
+    [Authorize] // Hela denna controllern kräver autentisering
     [Route("api/[controller]")]
     [ApiController]
     public class ProfileController : ControllerBase
     {
         private readonly ProfileService _profileService;
+        private readonly PlacesService _placesService;
 
-        public ProfileController(ProfileService profileService)
+        public ProfileController(ProfileService profileService, PlacesService placesService)
         {
             _profileService = profileService;
+            _placesService = placesService;
         }
 
         [HttpGet]
@@ -28,6 +30,20 @@ namespace Backend.Controllers
 
             var profile = await _profileService.GetBasicProfileInfoAsync(userId);
             return Ok(profile);
+        }
+
+        [HttpGet("visited-places")]
+        public async Task<ActionResult<List<VisitedPlaceDTO>>> GetVisitedPlaces()
+        {
+            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (!int.TryParse(userIdString, out int userId)) // Variabeln userId skapas bara om userIdString går att omvandla till en int
+            {
+                return Unauthorized("Invalid user id");
+            }
+
+            var visitedPlaces = await _placesService.GetVisitedPlacesByUserIdAsync(userId);
+            return Ok(visitedPlaces);
         }
     }
 }
