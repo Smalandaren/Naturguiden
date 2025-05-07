936a7bc83e6a82bd0eff112d61e447729307626c
Smalandaren <isakthysell05@gmail.com>
2025-05-07 18:48:16 +0200

    Ändrade zoomnivå på karta

--- Frontend/components/Map.tsx
+++ Frontend/components/Map.tsx
@@ -10,7 +10,7 @@ import { Place } from "@/types/Place";
 
 export default function Map({ place }: { place: Place }){
     return <div>
-        <MapContainer center={[place.latitude, place.longitude]} zoom={11} scrollWheelZoom={false} className="h-100 w-full justify-self-center px-10 m-0 z-0 rounded-xl border">
+        <MapContainer center={[place.latitude, place.longitude]} zoom={10} scrollWheelZoom={false} className="h-100 w-full justify-self-center px-10 m-0 z-0 rounded-xl border">
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
