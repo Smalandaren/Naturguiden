86077764cc5b4b8aa85022fcec83ee3f691f825a
MagiicalMae <ellenmae.lantz@gmail.com>
2025-05-08 20:06:08 +0200

    Gjorde så man kan se vänförfrågningar

--- Frontend/app/profile/(sidebar-items)/friends/FriendCard.tsx
+++ Frontend/app/profile/(sidebar-items)/friends/FriendCard.tsx
@@ -1,6 +1,11 @@
 import { Friend } from "@/types/Friend";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { format } from "date-fns";
+import { Button } from "@/components/ui/button";
+import {
+    CircleCheck,
+    CircleX
+  } from "lucide-react";
 
 export function FriendCard({friend}: {friend: Friend}) {
     return (    
@@ -26,6 +31,43 @@ export function FriendCard({friend}: {friend: Friend}) {
             </p>
           </div>
         </CardContent>
+      </Card>
+    );
+}
+
+export function FriendReqCard({friend}: {friend: Friend}) {
+    return (    
+    <Card className="w-full gap-3">
+        <div className="flex items-center px-3 gap-3">
+            <div>
+                <CardHeader className="flex flex-row items-center justify-between">
+                <div className="flex flex-row gap-2 items-center">
+                    <CardTitle className="text-xl font-bold">
+                    {friend.firstName} {friend.lastName}
+                    </CardTitle>
+                </div>
+
+                </CardHeader>
+                <CardContent className="space-y-3">
+                <div>
+                    <p className="text-sm text-muted-foreground">E-postadress</p>
+                    <p className="font-medium">{friend.email}</p>
+                </div>
+        
+                <div>
+                    <p className="text-sm text-muted-foreground">Förfrågan skickad</p>
+                    <p className="font-medium">
+                    {format(new Date(friend.requestTime), "d MMMM yyyy")}
+                    </p>
+                </div>
+                </CardContent>
+            </div>
+
+            <div className="flex flex-col items-center gap-5">
+                <Button><CircleCheck /></Button>
+                <Button><CircleX /></Button>
+            </div>
+        </div>
       </Card>
     );
 }
--- Frontend/app/profile/(sidebar-items)/friends/page.tsx
+++ Frontend/app/profile/(sidebar-items)/friends/page.tsx
@@ -2,13 +2,14 @@
 import { Friend } from "@/types/Friend";
 import { useEffect, useState } from "react";
 import { ErrorScreen } from "@/components/ErrorScreen";
-import { FriendCard } from "./FriendCard";
+import { FriendCard, FriendReqCard } from "./FriendCard";
 import { Separator } from "@/components/ui/separator";
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export default function FriendsPage() {
     const [isLoading, setIsLoading] = useState(true);
     const [friends, setFriends] = useState<Friend[]>();
+    const [friendRequests, setFriendRequests] = useState<Friend[]>();
 
     async function returnFriends(): Promise<Friend[] | null> {
         try {
@@ -43,11 +44,44 @@ export default function FriendsPage() {
         getFriends();
       }, []);
 
+    async function returnFriendRequests(): Promise<Friend[] | null> {
+        try {
+            const response = await fetch(`${apiUrl}/friends/get-requests`, {
+              cache: "no-cache",
+              method: "GET",
+              credentials: "include",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+      
+            const json = await response.json();
+            return json;
+        } catch (error) {
+            console.log(error);
+            return null;
+        } 
+    }
+
+    async function getFriendRequests() {
+        setIsLoading(true);
+        const requests = await returnFriendRequests();
+
+        if (requests) {
+            setFriendRequests(requests)
+        }
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        getFriendRequests();
+      }, []);
+      
       if (isLoading) {
         return <p>loading</p>;
       }
 
-      if (friends == null) {
+      if (friends == null || friendRequests == null) {
         return (
           <ErrorScreen
             title="Vänner kunde inte visas"
@@ -56,7 +90,7 @@ export default function FriendsPage() {
         );
       }
 
-      if (friends != null && friends.length < 1) {
+      if ((friends != null && friends.length < 1) && (friendRequests != null && friendRequests.length < 1)) {
         return (
           <ErrorScreen
             title="Inga vänner"
@@ -68,13 +102,28 @@ export default function FriendsPage() {
 
       return (
         <div className="mx-6 pt-16">
+
+        {friendRequests.length > 0 ? (
+            <>
+            <h1 className="text-3xl font-bold mb-4">Vänförfrågningar</h1>
+            <Separator />
+            <div className="flex flex-col gap-6 mt-4 max-w-2xl">
+            {friendRequests.map((friend) => {
+              return <FriendReqCard key={friend.id} friend={friend} />;
+            })}
+            </div>
+            <br />
+            </>) : (<></>)}
+        
+        {friends.length > 0 ? (
+          <>
           <h1 className="text-3xl font-bold mb-4">Mina vänner</h1>
           <Separator />
           <div className="flex flex-col gap-6 mt-4 max-w-2xl">
             {friends.map((friend) => {
               return <FriendCard key={friend.id} friend={friend} />;
             })}
-          </div>
+          </div></>) : (<></>)}
         </div>
       );
 }
