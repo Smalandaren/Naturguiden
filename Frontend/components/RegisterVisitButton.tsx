61f828d72e6e312541dce522c91f8692446b44b8
ThorANilsson <thor.a.nilsson@gmail.com>
2025-06-07 22:23:26 +0200

    Lyckat-meddelande visas nu för platsbesök och wishlist

--- Frontend/components/RegisterVisitButton.tsx
+++ Frontend/components/RegisterVisitButton.tsx
@@ -7,117 +7,135 @@ import { get } from "http";
 import { error } from "console";
 import { resolve } from "path";
 import { pl } from "date-fns/locale";
-import {
-    Circle,
-    CircleCheckBig
-} from "lucide-react";
+import { Circle, CircleCheckBig } from "lucide-react";
+import { toast } from "sonner";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-export default function RegisterVisitButton({place, user, text}: {place: Place, user: ProfileBasics | null, text:boolean}) {
-    const [isVisited, setIsVisited] = useState(Boolean);
+export default function RegisterVisitButton({
+  place,
+  user,
+  text,
+}: {
+  place: Place;
+  user: ProfileBasics | null;
+  text: boolean;
+}) {
+  const [isVisited, setIsVisited] = useState(Boolean);
 
-    useEffect(() => {
-     getIsVisited().then(val => setIsVisited(val))
-    },[])
+  useEffect(() => {
+    getIsVisited().then((val) => setIsVisited(val));
+  }, []);
 
-    async function getIsVisited(): Promise<boolean> {
-        try{
-            if (user === null) {
-                return false;
-            }
+  async function getIsVisited(): Promise<boolean> {
+    try {
+      if (user === null) {
+        return false;
+      }
 
-            const response = await fetch(`${apiUrl}/visits/check-visit`, {
-                method: "POST",
-                credentials: "include",
-                body: JSON.stringify({
-                    UserId: user.id,
-                    PlaceId: place.id 
-                }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                });
-        
-                if (!response.ok) {
-                    throw new Error(`Ett fel uppstod`);
-                }
+      const response = await fetch(`${apiUrl}/visits/check-visit`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify({
+          UserId: user.id,
+          PlaceId: place.id,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-                const json = await response.json();
-                return json;
-        } catch (error:any) {
-            console.log(error)
-            return false;
-        }
+      if (!response.ok) {
+        throw new Error(`Ett fel uppstod`);
+      }
+
+      const json = await response.json();
+      return json;
+    } catch (error: any) {
+      console.log(error);
+      return false;
     }
+  }
 
-    async function HandleClick() {
-        if (isVisited == false && user != null) {            
-            try {
-                const response = await fetch(`${apiUrl}/visits`, {
-                method: "POST",
-                credentials: "include",
-                body: JSON.stringify({
-                    PlaceId: place.id,
-                    UserId: user.id
-                }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                });
+  async function HandleClick() {
+    if (isVisited == false && user != null) {
+      try {
+        const response = await fetch(`${apiUrl}/visits`, {
+          method: "POST",
+          credentials: "include",
+          body: JSON.stringify({
+            PlaceId: place.id,
+            UserId: user.id,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-                if (response.ok) {
-                    setIsVisited(true);
-                }
-
-                if (!response.ok) {
-                throw new Error(`Ett fel uppstod`);
-                }
-                
-            } catch (error: any) {
-                console.log(error);
-            }
-        } else if (isVisited == true && user != null) {
-            try {
-                const response = await fetch(`${apiUrl}/visits`, {
-                method: "DELETE",
-                credentials: "include",
-                body: JSON.stringify({
-                    PlaceId: place.id, 
-                    UserId: user.id
-                }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                });
-
-                if (response.ok) {
-                    setIsVisited(false);
-                }
-
-                if (!response.ok) {
-                throw new Error(`Ett fel uppstod`);
-                }
-                
-            } catch (error: any) {
-                console.log(error);
-            }
+        if (response.ok) {
+          setIsVisited(true);
+          toast.success("Platsen har markerats som besökt");
         }
+
+        if (!response.ok) {
+          throw new Error(`Ett fel uppstod`);
+        }
+      } catch (error: any) {
+        toast.error("Platsen kunde inte markeras som besökt");
+        console.log(error);
+      }
+    } else if (isVisited == true && user != null) {
+      try {
+        const response = await fetch(`${apiUrl}/visits`, {
+          method: "DELETE",
+          credentials: "include",
+          body: JSON.stringify({
+            PlaceId: place.id,
+            UserId: user.id,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          setIsVisited(false);
+          toast.success("Platsen har avmarkerats som besökt");
+        }
+
+        if (!response.ok) {
+          throw new Error(`Ett fel uppstod`);
+        }
+      } catch (error: any) {
+        toast.error("Platsen kunde inte avmarkeras som besökt");
+        console.log(error);
+      }
     }
-    
-    return <>
-    {
-        user === null ? (
-            <></>
-        ) : (
-            
-        <Button onClick={event => {
+  }
+
+  return (
+    <>
+      {user === null ? (
+        <></>
+      ) : (
+        <Button
+          onClick={(event) => {
             event.preventDefault();
             HandleClick();
-            }}>
-            {text == true ? (<>Besökt</>) : (<></>)} {isVisited == false ? (<><Circle/></>) : (<><CircleCheckBig/></>)} 
+          }}
+        >
+          {text == true ? <>Besökt</> : <></>}{" "}
+          {isVisited == false ? (
+            <>
+              <Circle />
+            </>
+          ) : (
+            <>
+              <CircleCheckBig />
+            </>
+          )}
         </Button>
-    )}
+      )}
     </>
+  );
 }
-
